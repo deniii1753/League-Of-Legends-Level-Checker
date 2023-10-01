525bbe05d9d06565713ed60eab6f3ffda435c6a8
deniii1753 <deniii1753@gmail.com>
2023-10-01 21:48:29 +0300

    Add reading accounts from another file

---
+++ index.js
@@ -0,0 +1,3 @@
+// axios.get(`https://www.op.gg/_next/data/rqrYpMAq4Z_yEQbECOXJk/en_US/summoners/euw/${encodeURIComponent('Hi im Sparky asdasdasdv2a')}.json`)
+//     .then(res => console.log(res.data.pageProps.data.level))
+//     .catch(err => console.log(err.message));
