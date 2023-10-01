5f0ade8b7342bbcadc78fbae5f4b6a727075dee7
deniii1753 <deniii1753@gmail.com>
2023-10-01 21:59:58 +0300

    Add level showing for each account

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const axios = require('axios');
+const accounts = require('./accounts.json')
+
+for (const account of accounts) {
+    axios.get(`https://www.op.gg/_next/data/rqrYpMAq4Z_yEQbECOXJk/en_US/summoners/${account.region}/${encodeURIComponent(account.name)}.json`)
+        .then(res => console.log(res.data.pageProps.data.level))
+        .catch(err => console.log(err.message));
+}
+
 // axios.get(`https://www.op.gg/_next/data/rqrYpMAq4Z_yEQbECOXJk/en_US/summoners/euw/${encodeURIComponent('Hi im Sparky asdasdasdv2a')}.json`)
 //     .then(res => console.log(res.data.pageProps.data.level))
 //     .catch(err => console.log(err.message));
