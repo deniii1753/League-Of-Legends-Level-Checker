cc498a61341a069697426143e3501acd0d63fdee
deniii1753 <deniii1753@gmail.com>
2023-10-02 22:46:13 +0300

    Fixed the account removing bug when account's name was changed

--- src/accountsService.js
+++ src/accountsService.js
@@ -45,15 +45,15 @@ async function getAccountDetails(account) {
         const timePassed = currentDate - date;
 
         let time = '';
-        
-        if (timePassed / 8.64e+7 >= 1) { 
+
+        if (timePassed / 8.64e+7 >= 1) {
             time = `${(timePassed / 8.64e+7).toFixed(0)} days ago.`
-        } else if (timePassed / 3.6e+6 >= 1) { 
+        } else if (timePassed / 3.6e+6 >= 1) {
             time = `${(timePassed / 3.6e+6).toFixed(0)} hours ago.`
-        } else if (timePassed / 60000 >= 1) { 
+        } else if (timePassed / 60000 >= 1) {
             time = `${(timePassed / 60000).toFixed(0)} minutes ago.`;
         } else {
-            time = `${(timePassed / 1000).toFixed(0)} seconds ago.`; 
+            time = `${(timePassed / 1000).toFixed(0)} seconds ago.`;
         }
         return {
             name: res.data.pageProps.data.name,
@@ -71,20 +71,34 @@ function removeAccount(account) {
         try {
             const detailedAccount = await axios.get(`https://www.op.gg/_next/data/rqrYpMAq4Z_yEQbECOXJk/en_US/summoners/${account.region}/${encodeURIComponent(account.name)}.json`);
 
-            if (!detailedAccount.data.pageProps.data.name) return reject({ message: 'The account does not exist!' });
-
             fs.readFile('accounts.json', 'utf-8', (err, rawAccounts) => {
                 if (err) return reject(err.message);;
 
                 const accounts = JSON.parse(rawAccounts);
-                if (!accounts.find(x => x.summonerId === detailedAccount.data.pageProps.data.summoner_id)) return reject({ message: 'The account is not in the list!' });
+                let filteredAccounts = [];
 
-                fs.writeFile('accounts.json', JSON.stringify(accounts.filter(x => x.summonerId !== detailedAccount.data.pageProps.data.summoner_id)), 'utf-8', (err) => {
+                if (!detailedAccount.data.pageProps.data.name) {
+                    const accountToRemoveIndex = accounts.findIndex(x => x.name.toLowerCase() === account.name.toLowerCase() && x.region.toLowerCase() == account.region.toLowerCase());
+
+                    if (accountToRemoveIndex !== -1) {
+                        const firstPart = accounts.slice(0, accountToRemoveIndex);
+                        const lastPart = accounts.slice(accountToRemoveIndex + 1, accounts.length);
+
+                        filteredAccounts = firstPart.concat(lastPart);
+                    } else {
+                        return reject({ message: 'The account does not exist!' });
+                    }
+                } else {
+                    if (!accounts.find(x => x.summonerId === detailedAccount.data.pageProps.data.summoner_id)) return reject({ message: 'The account is not in the list!' });
+                    filteredAccounts = accounts.filter(x => x.summonerId !== detailedAccount.data.pageProps.data.summoner_id)
+                }
+
+                fs.writeFile('accounts.json', JSON.stringify(filteredAccounts), 'utf-8', (err) => {
                     if (err) return reject({ message: `An error occured when trying to remove the account! \n Error: ${err.message}` });
 
                     return resolve({
-                        name: detailedAccount.data.pageProps.data.name,
-                        region: detailedAccount.data.pageProps.region
+                        name: account.name,
+                        region: account.region.toUpperCase()
                     });
                 })
             });
@@ -119,12 +133,12 @@ function getAccounts(region) {
                 try {
                     await axios.post(`https://op.gg/api/v1.0/internal/bypass/summoners/${account.region}/${account.summonerId}/renewal`);
                     const checkedAccount = await getAccountDetails(account);
-                    if(checkedAccount.hasOwnProperty('name')) {
+                    if (checkedAccount.hasOwnProperty('name')) {
                         updatedAccounts.push(checkedAccount)
                     } else {
                         console.log(`An error occured while trying to get information about ${account.name} in ${account.region}! Skipping this account...`);
                     }
-                    
+
                 } catch (err) {
                     return reject(err.message);
                 }
